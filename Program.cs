3affe7f21fd39e757e04a135c423ff507a54ffbc
KyawHtay <Kyaw.Htay@gmail.com>
2021-05-19 00:12:09 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringCalculator cal = new StringCalculator();
+            int result=cal.Add("//;\n1;-2");
+        }
+    }
+}
